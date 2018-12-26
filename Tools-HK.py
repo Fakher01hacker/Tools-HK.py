8564056c54779685a252ec0c6294cd22919bab87
Fakher01hacker <46168697+Fakher01hacker@users.noreply.github.com>
2018-12-26 21:04:40 +0100

    Add files via upload
    Tools-HK

---
+++ Tools-HK.py
@@ -0,0 +1,151 @@
+# by amin hackerlearn-ngrok
+import sys
+import os
+import time
+A = '\033[1;30m'
+B = '\033[1;31m'
+C = '\033[1;32m'
+D = '\033[1;33m'
+E = '\033[1;34m'
+F = '\033[1;35m'
+os.system('clear')
+print(E)
+os.system('echo By Par Amin HaCkinG.........../')
+time.sleep(3)
+os.system('clear')
+print(C)
+os.system('echo By Par Amin HaCkinG............/')
+time.sleep (3)
+os.system('clear')
+print(F)
+os.system('echo By Par Amin HaCkinG............./')
+time.sleep (3)
+os.system('clear')
+print(A)
+os.system('echo By Par Amin HaCkinG............../')
+time.sleep (3)
+os.system('clear')
+print(D)
+os.system('echo By Par Amin HaCkinG.............../')
+time.sleep (3)
+os.system('clear')
+print(B)
+os.system('echo By Par Amin HaCkinG................/')
+time.sleep (3)
+os.system('clear')
+
+
+print(F)
+b = """
+ .... NO! ...                  ... MNO! ...    
+   ..... MNO!! ...................... MNNOO! ...    
+ ..... MMNO! ......................... MNNOO!! .    
+..... MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!! .    
+ ... !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO! ....    
+    ...... ! MMMMMMMMMMMMMPPPPOOOOIII! ! ...    
+   ........ MMMMMMMMMMMMPPPPPOOOOOOII!! .....    
+   ........ MMMMMOOOOOOPPPPPPPPOOOOMII! ...         
+    ....... MMMMM Amin OPPMM Rifi   OMI! ....    
+     ...... MMMM::   o.,OPMP,.o   ::I!! ...    
+         .... NNM:::.,,OOPM!P,.::::!! ....    
+          .. MMNNNNNOOOOPMO!!IIPPO!!O! .....    
+         ... MMMMMNNNNOO:!!:!!IPPPPOO! ....        
+           .. MMMMMNNOOMMNNIIIPPPOO!! ......    
+          ...... MMMONNMMNNNIIIOO!..........    
+       ....... MN MOMMMNNNIIIIIO! OO ..........    
+    ......... MNO! IiiiiiiiiiiiI OOOO ...........    
+  ...... NNN.MNO! . O!!!!!!!!!O . OONO NO! ........     
+   .... MNNNNNO! ...OOOOOOOOOOO .  MMNNON!........    
+   ...... MNNNNO! .. PPPPPPPPP .. MMNON!........    
+      ...... OO! ................. ON! .......    
+"""
+print(b)
+print(A)
+print ('##########《 1-Metasploit 》##########')
+print ('##########《 2-payload5 》##########')
+print ('##########《 3-Ngrok 》##########')
+print ('##########《 4-Nmap 》##########')
+print ('##########《 5-WOLF 》##########')
+print ('##########《 6-TMX 》##########')
+print ('##########《 7-checkIP 》##########')
+print ('##########《 8-MBF 》##########')
+print(E)
+print ('##########《 9-OSIF 》##########')
+print ('##########《 10-Cracker 》##########')
+print ('##########《 11-KaliNethunter 》##########')
+print ('##########《 12- Kali-Lunix》##########')
+print ('##########《 13-EasY_HaCk》##########')
+print ('##########《 14-TermuxAlpine》##########')
+print ('##########《 15-TermuxFedora》##########')
+print ('##########《 01- Download all scripts 》##########')
+print ('                《99- exit》')
+print(D)
+a=str(input('Enter The Number Tools :'))
+if a == '1':
+ print ('Downloading MetaSploit...')
+ time.sleep (2)
+ os.system('git clone https://github.com/verluchie/termux-metasploit')
+if a == '2':
+ print ('Downloading payload5...')
+ time.sleep(2)
+ os.system('git clone https://github.com/Ali898989/setupp ')
+if a == '3':
+ print ('Downloading Ngrok...')
+ time.sleep(2)
+ os.system('git clone https://github.com/inconshreveable/ngrok')
+if a == '4':
+ print ('Downloading Nmap...')
+ time.sleep (2)
+ os.system('git clone https://github.com/nmap/nmap')
+if a == '5':
+ print ('Downloading WOLF...') 
+ time.sleep (2)
+ os.system('git clone https://github.com/yehia-hacker/WOLF && ls && cd WOLF && python3 WOLF.py')
+if a == '6':
+ print ('Downloading TMX...')
+ time.sleep (2)
+ os.system('git clone https://github.com/MoMuilWH/TmX-FW && cd TmX-FW && ls && python2 TMX.py')
+if a == '7':
+ print ('Downloading checkIP...')
+ time.sleep (2)
+ os.system('git clone https://github.com/rebaiAla/checkIP.git &&cd checkIP && ls && python2 FBIP.py')
+if a == '8':
+ print ('Downloading MBF...')
+ time.sleep (2)
+ os.system('git clone https://github.com/YukersCreew/mbf')
+ os.system('ls && cd mbf && python2 MBF.py')
+if a == '9':
+ print ('Downloading OSIF...')
+ time.sleep (2)
+ os.system('git clone https://github.com/CiKu370/OSIF && ls && cd OSIF && ls && python2 osif.py')
+if a == '10' :
+ print ('Downloading Cracker...')
+ time.sleep (2)
+ os.system('git clone https://github.com/Mr-NemrMedo/Cracker && ls && cd Cracker && python2 Cracker.py')
+if a == '11':
+ print ('Downloading KaliNetHunter ON TERMUX...')
+ time.sleep (2)
+ os.system('git clone https://github.com/Hax4us/Nethunter-In-Termux')
+if a == '12':
+ print ('Downloading Kali-Lunix ON TERMUX...')
+ time.sleep (2)
+ os.system('git clone https://github.com/Techzindia/Kali-linux_For_Termux')
+if a == '13':
+ print ('Downloading EasY_HaCk...')
+ time.sleep (2)
+ os.system('git clone https://github.com/sabri-zaki/EasY_HaCk')
+if a == '14':
+ print ('Downloading TermuxAlpine...')
+ time.sleep (2)
+ os.system('git clone https://github.com/Hax4us/TermuxAlpine')
+if a == '15' :
+ print ('Downloading Termux-Fedora...')
+ time.sleep (2)
+ os.system('git clone https://github.com/nmilosev/termux-fedora')
+ os.sys(exit)
+if a == '01' :
+ print ('This All Scripts Downloading...')
+ time.sleep (2)
+ os.system('git clone https://github.com/verluchie/termux-metasploit && git clone https://github.com/Ali898989/setupp && git clone https://github.com/inconshreveable/ngrok && git clone https://github.com/nmap/nmap && git clone https://github.com/yehia-hacker/WOLF && git clone https://github.com/MoMuilWH/TmX-FW && git clone https://github.com/rebaiAla/checkIP.git && git clone https://github.com/YukersCreew/mbf && git clone https://github.com/CiKu370/OSIF && git clone https://github.com/Mr-NemrMedo/Cracker && git clone https://github.com/Hax4us/Nethunter-In-Termux && git clone https://github.com/Techzindia/Kali-linux_For_Termux && git clone https://github.com/sabri-zaki/EasY_HaCk && git clone https://github.com/Hax4us/TermuxAlpine && git clone https://github.com/termux-fedora')
+if a == '99':
+ os.system('exit')
